6dfe012766b6e7e8e88319128ab1a9cbaccc1916
Shalom <shalomjsph@gmail.com>
2020-09-03 07:46:23 +0100

    quiz first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+
+  
+   <div>
+    <div class="header">   <b> Fancy Quiz App </b>     
+   </div>
+      <b-nav>
+          <b-nav-item disabled> Counter: {{numCorrect}}/{{numTotal}} </b-nav-item>
+     </b-nav>
+   </div>
+</template>
+
+<script>
+export default {
+   props: [
+      'numCorrect',
+      'numTotal'
+   ]
+}
+</script>
+<style>
+.header{
+   font-family: perpetual;
+   font-size: 60px;
+   color: #255780;
+}
+</style>
